916742cf7366a3c85880b08c53b7208634b627b3
evertonfoz <evertoncoimbra@gmail.com>
2021-07-17 14:27:38 -0300

    1427

--- capitulo_03/ec_delivery/lib/core/presentation/theme.dart
+++ capitulo_03/ec_delivery/lib/core/presentation/theme.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
+const kTextColor = Colors.white;
+
 ThemeData theme() {
   return ThemeData(
     appBarTheme: AppBarTheme(backgroundColor: Colors.indigo.shade900),
     scaffoldBackgroundColor: Colors.indigo.shade600,
+    textTheme: textTheme(),
+  );
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    // bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+    // subtitle1: TextStyle(color: kTextColor),
   );
 }
